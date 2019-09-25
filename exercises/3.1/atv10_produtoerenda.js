deace4d4f7a2d2b9014589a222bb79c63f49cc0e
Richard Wellerson <richardwellerson@gmail.com>
2019-09-25 15:41:19 -0300

    Alterando Bug

--- exercises/3.1/atv10_produtoerenda.js
+++ exercises/3.1/atv10_produtoerenda.js
@@ -1,9 +1,14 @@
-custoProduto = 1000
-valorVenda = 1500
+custoProduto = 15.47
+porcentagemLucroProd = 0.5
+valorVenda = (custoProduto+(custoProduto*porcentagemLucroProd))
 quantidadeVenda = 1000
 custoRealProduto = (custoProduto+(custoProduto*0.2))
 custoTotalProduto = (custoRealProduto * quantidadeVenda)
 vendaTotal = (valorVenda * quantidadeVenda)
 lucroTotal = (vendaTotal - custoTotalProduto)
 
-console.log("O lucro total foi de R$ "+lucroTotal+",00.")
+if(custoProduto > 0){
+    console.log("O lucro total foi de R$ "+lucroTotal+",00.")    
+} else {
+    console.log("O valor não pode ser igual a zero.")
+}
