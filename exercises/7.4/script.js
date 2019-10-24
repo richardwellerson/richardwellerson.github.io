75f26f22dc0d6bd09162f3931893457b27fdfe78
Richard Wellerson <richardwellerson@gmail.com>
2019-10-24 20:09:57 -0300

    Modificando a posiçao do elemento de criaçao do span

--- exercises/7.4/script.js
+++ exercises/7.4/script.js
@@ -1,7 +1,6 @@
 let divReceiveCards = document.querySelector(".receive-deck")
 let initialDeck = document.querySelector(".deck")
 
-
 function insertCard (){
     initialDeck.addEventListener("click", function(){
         let generateCard = document.createElement("span")
