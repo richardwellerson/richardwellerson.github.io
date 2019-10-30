3e5fa3405de52a71b26622f159e0e3b472482b63
Richard Wellerson <richardwellerson@gmail.com>
2019-10-30 15:40:46 -0300

    Pequena alteraçao no exercicio 4

--- exercises/8.1/2/4.js
+++ exercises/8.1/2/4.js
@@ -4,4 +4,6 @@ skills = ["Linux","CSS","HTML","Bash","Shell"]
 console.log(`Tryber ${tryber} aqui! 
 Tudo bem?
 Minhas 5 maiores habilidades são:
-${skills.sort()}`)
+${skills.sort()}.
+#goTrybe
+#aleluiaQueEuEntendiEsseNegócio`)
