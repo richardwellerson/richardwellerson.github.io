f85a2d0de97667d6e249a011a4e626a51718ae62
Richard Wellerson <richardwellerson@gmail.com>
2019-11-06 20:14:43 -0300

    Iteraçao feita

--- exercises/9.1/script.js
+++ exercises/9.1/script.js
@@ -1,6 +1,26 @@
 let smallFont = document.querySelector('.small-font')
 let largeFont = document.querySelector('.large-font')
+let textColor = document.getElementById('font')
+let articleParagraphs = document.querySelectorAll('article p')
 
-let textColor = document.getElementById('font').value
+function changeTextColor() {
+    for(let i = 0; i < articleParagraphs.length; i++) {
+        textColor.addEventListener('input', () => {
+            localStorage.setItem('color', textColor.value)
+            articleParagraphs[i].style.color = localStorage.color
+        })
+    }
+}
+
+changeTextColor()
 
 
+// (function () {
+    // const selectNimbus = document.querySelector(".nimbus")
+    // const paragraphToGrow = document.querySelectorAll(".grow-paragraph")
+    // selectNimbus.addEventListener("click", () => selectNimbus.style.animationPlayState = "running")
+    // selectNimbus.addEventListener("dblclick", () => selectNimbus.style.animationPlayState = "paused")
+    // for(let i = 0; i < paragraphToGrow.length; i++) {
+        // paragraphToGrow[i].addEventListener('click', () => paragraphToGrow[i].style.animationPlayState = "running")
+    // }
+// }())
