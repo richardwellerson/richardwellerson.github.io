aff4ef04d256b7b1ad60e2ae95544f2f76729114
Richard Wellerson <richardwellerson@gmail.com>
2019-11-07 15:13:22 -0300

    Funçao de cor das letras feita

--- exercises/9.1/script.js
+++ exercises/9.1/script.js
@@ -5,22 +5,12 @@ let articleParagraphs = document.querySelectorAll('article p')
 
 function changeTextColor() {
     for(let i = 0; i < articleParagraphs.length; i++) {
+        articleParagraphs[i].style.color = localStorage.color
         textColor.addEventListener('input', () => {
             localStorage.setItem('color', textColor.value)
             articleParagraphs[i].style.color = localStorage.color
-        })
+        })  
     }
 }
 
 changeTextColor()
-
-
-// (function () {
-    // const selectNimbus = document.querySelector(".nimbus")
-    // const paragraphToGrow = document.querySelectorAll(".grow-paragraph")
-    // selectNimbus.addEventListener("click", () => selectNimbus.style.animationPlayState = "running")
-    // selectNimbus.addEventListener("dblclick", () => selectNimbus.style.animationPlayState = "paused")
-    // for(let i = 0; i < paragraphToGrow.length; i++) {
-        // paragraphToGrow[i].addEventListener('click', () => paragraphToGrow[i].style.animationPlayState = "running")
-    // }
-// }())
