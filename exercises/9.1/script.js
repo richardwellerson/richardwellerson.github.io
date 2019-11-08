06d99c7282802228bc04f77083f235b5fb21e75a
Richard Wellerson <richardwellerson@gmail.com>
2019-11-07 22:18:51 -0300

    background feito

--- exercises/9.1/script.js
+++ exercises/9.1/script.js
@@ -2,15 +2,24 @@ let smallFont = document.querySelector('.small-font')
 let largeFont = document.querySelector('.large-font')
 let textColor = document.getElementById('font')
 let articleParagraphs = document.querySelectorAll('article p')
+let backgroundColor = document.getElementById('background')
 
-function changeTextColor() {
+const changeColor = () => {
     for(let i = 0; i < articleParagraphs.length; i++) {
         articleParagraphs[i].style.color = localStorage.color
         textColor.addEventListener('input', () => {
             localStorage.setItem('color', textColor.value)
             articleParagraphs[i].style.color = localStorage.color
         })  
+        articleParagraphs[i].style.backgroundColor = localStorage.background
+        backgroundColor.addEventListener('input', () => {
+            localStorage.setItem('background', backgroundColor.value)
+            articleParagraphs[i].style.backgroundColor = localStorage.background
+        })
     }
 }
 
-changeTextColor()
+
+
+
+changeColor()
