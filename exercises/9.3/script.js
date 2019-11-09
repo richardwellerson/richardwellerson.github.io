f4f09d27608df54130eabafe4c8f1cfcda28c40d
Richard Wellerson <richardwellerson@gmail.com>
2019-11-08 21:37:56 -0300

    Exercício 1 feito

--- exercises/9.3/script.js
+++ exercises/9.3/script.js
@@ -1,10 +1,11 @@
-const API_URL = "https://icanhazdadjoke.com/"
-
+const h2Tag = (tagWrite) => document.getElementById(tagWrite)
 const fetchJoke = () => {
-    $.getJSON(API_URL,
-    (data) => {
-
-    }    
-    )}
+    const API_URL = "https://icanhazdadjoke.com/"
+    fetch(API_URL, {
+            headers: { Accept: "application/json" }
+        })
+        .then((response) => response.json())
+        .then((json) => h2Tag('joke-container').innerHTML = json.joke)
+}
 
 fetchJoke()
