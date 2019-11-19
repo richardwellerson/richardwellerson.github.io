5eba6d8ffeeeb586497f8952cf3c73cd3924094d
Richard Wellerson <richardwellerson@gmail.com>
2019-11-19 18:48:16 -0300

    Inserindo informaçoes do gabarito nos arquivos .css

--- exercises/my-pokedex/src/components/Pokemon.jsx
+++ exercises/my-pokedex/src/components/Pokemon.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
 
+class Pokemon extends React.Component {
+  render() {
+    const { pokemons } = this.props
+    console.log(pokemons)
+    return(
+      <p>Show</p>
+    )
+  }
+}
 
 export default Pokemon;
