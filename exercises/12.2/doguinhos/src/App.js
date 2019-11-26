c4621d89f846d7d3084972548efa576f516cd423
Richard Wellerson <richardwellerson@gmail.com>
2019-11-26 18:59:42 -0300

    retirando arquivo nao usado no projeto. Inserindo shouldComponentUpdate()

--- exercises/12.2/doguinhos/src/App.js
+++ exercises/12.2/doguinhos/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: 0
+    }
+    this.setNewNumber = this.setNewNumber.bind(this);
+  }
+
+  setNewNumber() {
+    this.setState({ data: this.state.data + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.setNewNumber}>INCREMENT</button>
+        <Content myNumber={this.state.data} />
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component {
+  shouldComponentUpdate() { return this.props.myNumber % 3 === 0 }
+  render() {
+    return (
+      <div>
+        <h3>{this.props.myNumber}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
