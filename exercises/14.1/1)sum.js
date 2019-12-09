03eac4321a1c6b76f4499668e36ec801785ec629
Richard Wellerson <richardwellerson@gmail.com>
2019-12-09 18:07:54 -0300

    14.1 | Exercicios

---
+++ exercises/14.1/1)sum.js
@@ -0,0 +1,11 @@
+let assert = require('assert')
+
+function sum(a, b) {
+  if (typeof(a) !== "number" || typeof(b) !== "number") {
+    throw new Error("parameters must be numbers")
+  }
+
+  return a + b
+}
+
+// implemente seus testes aqui
