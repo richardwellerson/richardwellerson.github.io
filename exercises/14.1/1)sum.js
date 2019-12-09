d89193222029d0a8d939515464a6ff08f442a903
Richard Wellerson <richardwellerson@gmail.com>
2019-12-09 19:15:12 -0300

    Exercicio confirmando erro

--- exercises/14.1/1)sum.js
+++ exercises/14.1/1)sum.js
@@ -8,4 +8,6 @@ function sum(a, b) {
   return a + b
 }
 
-// implemente seus testes aqui
+assert.strictEqual(sum(4,5),9)
+assert.strictEqual(sum(0,0),0)
+assert.throws(() => { sum(4,"5") }, /^Error: parameters must be numbers/)
