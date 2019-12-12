924621501b22317fdd655722fd00f0303720557c
Richard Wellerson <richardwellerson@gmail.com>
2019-12-12 18:57:34 -0300

    trabalhando no 4 exercicio

--- exercises/14.1/4)myRemove.js
+++ exercises/14.1/4)myRemove.js
@@ -8,8 +8,16 @@ function myRemove(arr, item) {
       ret.push(arr[i]);
     }
   }
-
+  
   return ret;
 }
 
-// implemente seus testes aqui
+assert.strictEqual(myRemove([1, 2, 3, 4], 3), [ 1, 2, 4 ])
+
+/*
+A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+Verique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+Verique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+Verique que o array passado por parâmetro não sofreu alterações
+Verique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.
+*/
