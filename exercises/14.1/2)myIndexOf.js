76ebcbdf11a27627a3dc7e4486213f30b9d27998
Richard Wellerson <richardwellerson@gmail.com>
2019-12-12 18:18:12 -0300

    Exercicio 2

---
+++ exercises/14.1/2)myIndexOf.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+
+function myIndexOf(arr, item) {
+  
+  return arr.findIndex(element => { 
+    return element === item })
+}
+
+assert.strictEqual(myIndexOf([1,2,3,4], 3), 2)
+assert.strictEqual(myIndexOf([1,2,3,4] ,5), -1)
