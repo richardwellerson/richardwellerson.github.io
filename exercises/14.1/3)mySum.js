26761a5c1a39a3164caa7638fef562a11d20e99f
Richard Wellerson <richardwellerson@gmail.com>
2019-12-12 18:23:51 -0300

    Exercicio 3 feito

--- exercises/14.1/3)mySum.js
+++ exercises/14.1/3)mySum.js
@@ -4,4 +4,5 @@ function mySum(arr) {
   return arr.reduce((total, current) => { return total + current }, 0)
 }
 
-// implemente seus testes aqui
+assert.strictEqual(mySum([1, 2, 3, 4]), 10)
+assert.strictEqual(mySum([1, -2, -3, 4]), 0)
