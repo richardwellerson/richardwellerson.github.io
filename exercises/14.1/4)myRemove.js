4d1c2d5c82d3d3ccf6a8860c0d1297a5fa4f4a5a
Richard Wellerson <richardwellerson@gmail.com>
2019-12-12 19:13:02 -0300

    Exercicio 4 feito

--- exercises/14.1/4)myRemove.js
+++ exercises/14.1/4)myRemove.js
@@ -8,16 +8,12 @@ function myRemove(arr, item) {
       ret.push(arr[i]);
     }
   }
-  
+
   return ret;
 }
 
-assert.deepEqual(myRemove([1, 2, 3, 4], 3), [ 1, 2, 4 ])
+assert.deepEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.deepEqual(myRemove([1, 2, 3, 4]), [1, 2, 3, 4]);
+assert.deepEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4])
 
-/*
-A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
-Verique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
-Verique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-Verique que o array passado por parâmetro não sofreu alterações
-Verique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.
-*/
