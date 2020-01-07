41bb375c2581f5a0eb4ac5a1573d41eed2fb0c76
richardwellerson <richardwellerson@gmail.com>
2020-01-07 18:07:01 -0300

    Inserindo componentes title e matrix no app

--- exercises/15.1/hash/src/App.js
+++ exercises/15.1/hash/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from './components/title';
+import Matrix from './components/matrix'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Matrix />
     </div>
   );
 }
