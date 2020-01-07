203d7d268d11a605c98fefeee178a29c11c552f6
richardwellerson <richardwellerson@gmail.com>
2020-01-07 18:07:19 -0300

    Feito base do jogo da velha

---
+++ exercises/15.1/hash/src/components/matrix.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Matrix extends React.Component {
+  render() {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td className="1.1"></td>
+            <td className="1.2"></td>
+            <td className="1.3"></td>
+          </tr>
+          <tr>
+            <td className="2.1"></td>
+            <td className="2.2"></td>
+            <td className="2.3"></td>
+          </tr>
+          <tr>
+            <td className="3.1"></td>
+            <td className="3.2"></td>
+            <td className="3.3"></td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+}
+
+export default Matrix;
