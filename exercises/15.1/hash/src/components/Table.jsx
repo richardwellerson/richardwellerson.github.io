6bcef8e10a1f1195c8daadabd8b030943aa21d3b
richardwellerson <richardwellerson@gmail.com>
2020-02-27 11:21:27 -0300

    Parado no exercicio com problemas no State

--- exercises/15.1/hash/src/components/Table.jsx
+++ exercises/15.1/hash/src/components/Table.jsx
@@ -5,34 +5,25 @@ class Table extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tab11: false,
-      tab12: false,
-      tab13: false,
-      tab21: false,
-      tab22: false,
-      tab23: false,
-      tab31: false,
-      tab32: false,
-      tab33: false,
       index: 0,
       hashValues: ['X', 'O'],
+      tab11: '',
+      tab12: '',
+      tab13: '',
+      tab21: '',
+      tab22: '',
+      tab23: '',
+      tab31: '',
+      tab32: '',
+      tab33: '',
     }
   }
 
-  changeStateValue(index, ...tabValue) {
-    if (!tabValue) {
-      this.setState({
-        index: index ? 0 : 1,
-        tabValue: true,
-      });
-      return this.state.hashValues[this.state.index];
-    }
-  }
-
-  printClick = (tabvalue) => {
-    if (tabvalue) {
-      return this.state.hashValues[this.state.index];
-    };
+  changeStateValue(index, tabValue) {
+    this.setState({
+      index: index ? 0 : 1,
+      tabValue: this.state.hashValues[index],
+    });
   }
 
   render() {
@@ -40,36 +31,37 @@ class Table extends React.Component {
       <table>
         <tbody>
           <tr>
-            <td className="Table_cell" onClick={() => this.changeStateValue(this.state.index, this.state.tab11)}>
-              <p className="Table_value_x-Or-o">{this.printClick(this.state.tab11)}</p>
+            <td
+              className="Table_cell" onClick={() => this.changeStateValue(this.state.index, this.state.tab11)}>
+              <p className="Table_value_x-Or-o">{this.state.tab11}</p>
             </td>
             <td className="Table_cell" onClick={() => this.changeStateValue(this.state.index, this.state.tab12)}>
-              <p className="Table_value_x-Or-o">{this.printClick(this.state.tab12)}</p>
+              <p className="Table_value_x-Or-o">{this.state.tab12}</p>
             </td>
             <td className="Table_cell" onClick={() => this.changeStateValue(this.state.index, this.state.tab13)}>
-              <p className="Table_value_x-Or-o">{this.printClick(this.state.tab13)}</p>
+              <p className="Table_value_x-Or-o">{this.state.tab13}</p>
             </td>
           </tr>
           <tr>
             <td className="Table_cell" onClick={() => this.changeStateValue(this.state.index, this.state.tab21)}>
-              <p className="Table_value_x-Or-o">{this.printClick(this.state.tab21)}</p>
+              <p className="Table_value_x-Or-o">{this.state.tab21}</p>
             </td>
             <td className="Table_cell" onClick={() => this.changeStateValue(this.state.index, this.state.tab22)}>
-              <p className="Table_value_x-Or-o">{this.printClick(this.state.tab22)}</p>
+              <p className="Table_value_x-Or-o">{this.state.tab22}</p>
             </td>
             <td className="Table_cell" onClick={() => this.changeStateValue(this.state.index, this.state.tab23)}>
-              <p className="Table_value_x-Or-o">{this.printClick(this.state.tab23)}</p>
+              <p className="Table_value_x-Or-o">{this.state.tab23}</p>
             </td>
           </tr>
           <tr>
             <td className="Table_cell" onClick={() => this.changeStateValue(this.state.index, this.state.tab31)}>
-              <p className="Table_value_x-Or-o">{this.printClick(this.state.tab31)}</p>
+              <p className="Table_value_x-Or-o">{this.state.tab31}</p>
             </td>
             <td className="Table_cell" onClick={() => this.changeStateValue(this.state.index, this.state.tab32)}>
-              <p className="Table_value_x-Or-o">{this.printClick(this.state.tab32)}</p>
+              <p className="Table_value_x-Or-o">{this.state.tab32}</p>
             </td>
             <td className="Table_cell" onClick={() => this.changeStateValue(this.state.index, this.state.tab33)}>
-              <p className="Table_value_x-Or-o">{this.printClick(this.state.tab33)}</p>
+              <p className="Table_value_x-Or-o">{this.state.tab33}</p>
             </td>
           </tr>
         </tbody>
